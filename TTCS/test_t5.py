fbe97987f297c958fba2c2bbb56f08b1678c1105
TungNsSon <mmr9grs@gmail.com>
2025-05-09 22:08:31 +0700

    Push to Pack

--- TTCS/test_t5.py
+++ TTCS/test_t5.py
@@ -2,5 +2,5 @@ from happytransformer import HappyTextToText, TTSettings
 
 happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
 args = TTSettings(num_beams=5, min_length=1)
-result = happy_tt.generate_text("grammar: I not good ", args=args)
+result = happy_tt.generate_text("grammar: I seen him yesterday", args=args)
 print("Generated text:", result.text)
